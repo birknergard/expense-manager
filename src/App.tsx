47ec4787d06ef49c761b02f0cc65959e30abfe66
Birk Kristinius Nergård <birknergard@gmail.com>
2024-12-16 19:33:16 +0100

    Created initial folder structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-
-function App() {
+ 
+const App = () => {
   return (
-    <div className="App">
-    </div>
-  );
+    <>
+    
+      <div className="App">
+
+      </div>
+    </>
+
+  )
 }
 
 export default App;
