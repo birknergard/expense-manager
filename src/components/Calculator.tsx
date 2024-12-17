924e24898fb9073c97ec4737fa59e3e1ce1ecb76
Birk Kristinius Nergård <birknergard@gmail.com>
2024-12-17 12:40:35 +0100

    Gross and net in Calculator comp. Adjustable preview in NumberInput.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,13 +3,50 @@ import NumberInput from "./NumberInput";
 
 const Calculator = () => {
 
-    const [baseRate, setBaseRate] = useState<number>(0)
+    const [baseHours, setRate] = useState<number>(0)
+    const [ub1Hours, setUb1Hours] = useState<number>(0)
+    const [ub2Hours, setUb2Hours] = useState<number>(0)
+    const [ub3Hours, setUb3Hours] = useState<number>(0)
+    const [ub4Hours, setUb4Hours] = useState<number>(0)
+
+    const gross = () : number => {
+        return (baseHours * 180) + (ub1Hours * 22) + (ub2Hours * 45) + (ub3Hours * 55) + (ub4Hours * 110);
+    }
+
+    const net = (tax : number) => {
+        return gross() * tax
+    }
+
+    useEffect(() => {
+
+    }, [baseHours, ub1Hours, ub2Hours, ub3Hours, ub4Hours])
 
     return(
         <div>
             <NumberInput addStyles="border border-black" 
-                value={baseRate}
-                setValue={setBaseRate}
+                value={baseHours}
+                setValue={setRate}
+                previewValue={180}
+            />
+            <NumberInput addStyles="border border-black" 
+                value={ub1Hours}
+                setValue={setUb1Hours}
+                previewValue={22}
+            />
+            <NumberInput addStyles="border border-black" 
+                value={ub2Hours}
+                setValue={setUb2Hours}
+                previewValue={45}
+            />
+            <NumberInput addStyles="border border-black" 
+                value={ub3Hours}
+                setValue={setUb3Hours}
+                previewValue={55}
+            />
+            <NumberInput addStyles="border border-black" 
+                value={ub4Hours}
+                setValue={setUb4Hours}
+                previewValue={110}
             />
         </div>
     )
--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -3,16 +3,27 @@ import React, { FC, Dispatch, SetStateAction } from "react";
 interface INumberInput {
     value : number,
     setValue : Dispatch<SetStateAction<number>>
+    previewValue : string
     addStyle? : string
 }
 
-const NumberInput : FC<INumberInput> = ({value, setValue, addStyle}) => {
+const NumberInput : FC<INumberInput> = ({
+    value, 
+    setValue, 
+    addStyle, 
+    previewValue
+}) => {
     return(
-        <input className={`p-1 rounded-mb ${addStyle}`}
-            value={value}
-            onChange = {(e) => setValue(e.current.value)}
-            type="number" 
-        />
+        <div className="flex flex-row">
+            <input className={`p-1 rounded-mb text-lg ${addStyle}`}
+                value={value}
+                onChange = {(e) => setValue(e.current.value)}
+                type="number" 
+            />
+            <h2>
+                * {previewValue}
+            </h2>
+        </div>
     );
 }
 
