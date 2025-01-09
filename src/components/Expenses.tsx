6624eed9626bc52c4c085936850d2eb759e928e5
Birk Kristinius Nergård <birknergard@gmail.com>
2025-01-10 00:56:39 +0100

    Created framework for ExpenseList component.

--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -1,22 +1,31 @@
-import React, { FC } from "react";
+import React, { Dispatch, FC, ReactNode, SetStateAction, useState } from "react";
 import { Expense } from "../interfaces/Models";
 
 interface IExpenseList{
-    total: number
+    totalSetter: Dispatch<SetStateAction<number>>
+}
+interface IExpenseItem{
+    expense : Expense
 }
 
-const ExpenseList : FC<IExpenseList> = ({total}) => {
+const ExpenseList : FC<IExpenseList> = ({totalSetter}) => {
 
-    
+    const [expenseList, setExpenseList] = useState<Expense[]>([])
 
+    const updateUI = () : React.JSX.Element[] => {
+        return expenseList.map((expense, index) => (
+           <ExpenseItem key={index} expense={expense} />  
+        ))
+    }
 
     return(
-        <>
-        </>
+        <div>
+            {updateUI()}
+        </div>
     );
 }
 
-const Expense : FC<Expense> = () => {
+const ExpenseItem : FC<IExpenseItem> = ({expense}) => {
 
 
     return(
